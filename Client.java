d70268e0b85426bf7074df5fef674f12d8c2e7e0
EthanAndMatt <eb110798@gmail.com>
2016-06-03 20:47:16 -0700

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,52 +5,65 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import javax.swing.JOptionPane;
+import javax.swing.JFrame;
 
 
-public class Client {
+public class Client extends JFrame {
 	
-	private static String IP = JOptionPane.showInputDialog("Enter an IP Address. "
-			+ "NOTE: TestBench IP = 10.140.131.236");
-	private static String Port = JOptionPane.showInputDialog("Enter a Port number.");
+	GraphicPanel fr;
 	
-	private static String[] nameAndPort = {IP, Port};
-	
-	public static void main(String[] args) throws IOException {
-	        
-		 args = nameAndPort;
-	        if (args.length != 2) {
-	            System.err.println(
-	                "Usage: java EchoClient <host name> <port number>");
-	            System.exit(1);
-	        }
+	public Client(String string) {
+		super(string);
+	}
 
-	        String hostName = args[0];
-	        int portNumber = Integer.parseInt(args[1]);
+	public static void main(String[] args){
+		Client c = new Client("Client Frame");
+		c.setUpUI();
+		c.pack();
+		c.setVisible(true);
+	}
 
-	        try (
-	            Socket echoSocket = new Socket(hostName, portNumber);
-	            PrintWriter out =
-	                new PrintWriter(echoSocket.getOutputStream(), true);
-	            BufferedReader in = new BufferedReader(
-	                    new InputStreamReader(echoSocket.getInputStream()));
-	            BufferedReader stdIn =
-	                new BufferedReader(
-	                    new InputStreamReader(System.in))
-	        ) {
-	            String userInput;
-	            System.out.println("echo: " + in.readLine());
-	            while ((userInput = stdIn.readLine()) != null) {
-	                out.println(userInput);
+	private void setUpUI() {
+		fr = new GraphicPanel(this);
+		fr.initializeIPP();
+		this.add(fr);
+		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+	}
+	public void connect(String ip, String port){
+		String[] nameAndPort = {ip, port};
+		if (nameAndPort.length != 2) {
+			fr.Error_usage();
+			fr.print("Usage: <host name> <port number>");
+		}
 
-	            }
-	        } catch (UnknownHostException e) {
-	            System.err.println("Don't know about host " + hostName);
-	            System.exit(1);
-	        } catch (IOException e) {
-	            System.err.println("Couldn't get I/O for the connection to " +
+		String hostName = nameAndPort[0];
+		int portNumber = Integer.parseInt(nameAndPort[1]);
+
+		try (
+				Socket s = new Socket(hostName, portNumber);
+				PrintWriter out =
+						new PrintWriter(s.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(
+						new InputStreamReader(s.getInputStream()));
+				BufferedReader stdIn =
+						new BufferedReader(
+								new InputStreamReader(System.in))
+				) {
+			String userInput;
+//			System.out.println("echo: " + in.readLine());
+			fr.print(in.readLine());
+			while ((userInput = stdIn.readLine()) != null) {
+				out.println(userInput);
+				
+			}
+		} catch (UnknownHostException e) {
+			fr.print("Don't know about host " + hostName);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to " +
 	                hostName);
-	            System.exit(1);
-	        } 
-	    }
+			System.exit(1);
+		} 
+		
+	}
 }
